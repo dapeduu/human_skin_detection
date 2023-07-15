6a77fdbc6975392e63e5af13d224ee51568c6d10
Pedro Santos <pedrosantosdevelop@gmail.com>
2023-07-15 11:50:14 -0300

    template do script

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+# Carregando imagens
+img=cv2.imread("./images/iluminacao_frontal.jpg")
+
+# Equalização de histograma
+
+# Suavização
+
+# Detecção HSV
+
+# Detecção YCbCr
+
+# Resultados
+
+
+cv2.waitKey(0) # Caso mostremos uma figura
+cv2.destroyAllWindows()
