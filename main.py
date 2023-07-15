67856405caa424d76040d873607cd64a00cee1a1
Pedro Santos <pedrosantosdevelop@gmail.com>
2023-07-15 18:48:12 -0300

    equalizacao de histograma

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 # Carregando imagens
-img=cv2.imread("./images/iluminacao_frontal.jpg")
+original = cv.imread("./images/input/1.jpg")
+assert original is not None, "file could not be read, check with os.path.exists()"
 
 # Equalização de histograma
+
+ycbcr = cv.cvtColor(original, cv.COLOR_BGR2YCrCb)
+ycbcr[:,:,0] = cv.equalizeHist(ycbcr[:, :,0]) # Equalizando somente o canal de brilho
 
 # Suavização
 
@@ -13,7 +17,8 @@ img=cv2.imread("./images/iluminacao_frontal.jpg")
 # Detecção YCbCr
 
 # Resultados
+cv.imshow('Original', original)
+cv.imshow('Equalized', cv.cvtColor(ycbcr, cv.COLOR_YCrCb2BGR))
 
-
-cv2.waitKey(0) # Caso mostremos uma figura
-cv2.destroyAllWindows()
+cv.waitKey(0) # Caso mostremos uma figura
+cv.destroyAllWindows()
