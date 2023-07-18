858f462f53a879d2138a37e732681cddc74308e1
Pedro Santos <pedrosantosdevelop@gmail.com>
2023-07-18 15:44:14 -0300

    escrevendo resultados

--- main.py
+++ main.py
@@ -7,6 +7,31 @@ import matplotlib.pyplot as plt
 original = cv.imread("./images/input/11.png")
 assert original is not None, "file could not be read, check with os.path.exists()"
 
+# Suavização
+# https://www.youtube.com/watch?v=YVBxM64kpkU&t=206s
+
+def low_pass_filter(image):
+    dft = cv.dft(np.float32(image),flags = cv.DFT_COMPLEX_OUTPUT)
+    dft_shift = np.fft.fftshift(dft)
+
+    rows, cols = image.shape
+    crow,ccol = rows//2 , cols//2
+
+    # create a mask first, center square is 1, remaining all zeros
+    mask = np.zeros((rows,cols,2),np.uint8)
+    mask[crow-25:crow+25, ccol-25:ccol+25] = 1
+
+    # apply mask and inverse DFT
+    fshift = dft_shift*mask
+    f_ishift = np.fft.ifftshift(fshift)
+    magnitude_spectrum = 20*np.log(np.abs(fshift))
+    img_back = cv.idft(f_ishift)
+    img_back = cv.magnitude(img_back[:,:,0],img_back[:,:,1])
+
+    filtered_magnitude_spectrum = cv.normalize(magnitude_spectrum, None, 0, 255, cv.NORM_MINMAX, dtype=cv.CV_8U)
+    filtered_image = cv.normalize(img_back, None, 0, 255, cv.NORM_MINMAX, dtype=cv.CV_8U)
+
+    return filtered_image
 
 # Detecção HSV
 def hsv_detection(bgr_img):
@@ -31,32 +56,30 @@ def get_final_mask(ycbcr_mask, hsv_mask):
     return result
 
 
+# Pre processamento
 ycbcr = cv.cvtColor(original, cv.COLOR_BGR2YCrCb)
-
 y_channel, cr_channel, cb_channel = cv.split(ycbcr)
 y_channel_equalized = cv.equalizeHist(y_channel) # Equalizando somente o canal de brilho
-ycbcr_equalized = cv.merge((y_channel_equalized, cr_channel, cb_channel))
-ycbcr_equalized_rgb = cv.cvtColor(original, cv.COLOR_YCrCb2RGB)
+low_pass = low_pass_filter(y_channel_equalized)
+ycbcr_low_passed = cv.merge((low_pass, cr_channel, cb_channel))
+ycbcr_equalized_rgb = cv.cvtColor(ycbcr_low_passed, cv.COLOR_YCrCb2RGB)
 
-hsv_mask = hsv_detection(original)
-ycbcr_mask = ycbcr_detection(original)
+# Detecção HSV e YCbCr
+hsv_mask = hsv_detection(ycbcr_equalized_rgb)
+ycbcr_mask = ycbcr_detection(ycbcr_equalized_rgb)
 
+# Junção dos resultados
 result = get_final_mask(ycbcr_mask, hsv_mask)
 skin_image = cv.bitwise_and(original, original, mask=result)
 
-# Resultados
+# Salvar resultados
 
-plt.subplot(121), plt.imshow(hsv_mask, cmap='gray')
-plt.title('HSV Resultado'), plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(ycbcr_mask, cmap='gray')
-plt.title('YCbCr Resultado'), plt.xticks([]), plt.yticks([])
-plt.show()
-
-plt.subplot(121), plt.imshow(result, cmap='gray')
-plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(skin_image, cmap='gray')
-plt.title('Resultado'), plt.xticks([]), plt.yticks([])
-plt.show()
+cv.imwrite(f"./images/results/1_y_channel_equalized.png", y_channel_equalized)
+cv.imwrite(f"./images/results/2_ycbcr_low_passed.png", ycbcr_low_passed)
+cv.imwrite(f"./images/results/3_hsv_mask.png", hsv_mask)
+cv.imwrite(f"./images/results/4_ycbcr_mask.png", ycbcr_mask)
+cv.imwrite(f"./images/results/5_result.png", result)
+cv.imwrite(f"./images/results/6_skin_image.png", skin_image)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
